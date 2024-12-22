452f2ceae10f5a259643f425ef5a955cd7718319
r3lax3rs <124855911+r3lax3rs@users.noreply.github.com>
2024-12-22 19:00:28 +0100

    Update PreInstall.sh

--- PreInstall.sh
+++ PreInstall.sh
@@ -2,9 +2,6 @@
 # This is a custom install script for Arch
 # This script is meant for Intel+Nvidia users
 #
-#First we will initialize pacman and populate it
-pacman-key --init
-pacman-key --populate archlinux
 #Editing certain lines in /etc/pacman.conf for personal use
 sed -i 's/#Color/Color/' /etc/pacman.conf
 sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 20/' /etc/pacman.conf
