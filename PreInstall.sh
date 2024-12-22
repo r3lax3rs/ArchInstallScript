15bb01fe4c26f93e435cb05804f5e509052742fe
r3lax3rs <124855911+r3lax3rs@users.noreply.github.com>
2024-12-22 13:33:58 +0100

    Add files via upload

--- PreInstall.sh
+++ PreInstall.sh
@@ -11,3 +11,12 @@ sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 20/' /etc/pacman.conf
 #Issues with installing Audio via default archinstall script; fix
 pacman -S pipewire wireplumber pipewire-pulse pipewire-alsa --noconfirm
 systemctl --user enable --now pipewire.service wireplumber.service pipewire-pulse.service
+#
+#Installing through the JSON user configuration from the archinstall script
+#First getting file through git
+#pacman -S git --needed --noconfirm
+#git user_configuration.json
+#
+#archinstall --config /path/to/user_configuration.json
+#--creds = to specify a credentials configuration file user_configuration.json
+#check how this works and if i can put it in a variable the user has to give (user+pw & create file + use it)
