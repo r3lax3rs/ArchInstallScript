32b7c11f6eabe5aa5d90c3ed5e66af1da3d55e6b
r3lax3rs <124855911+r3lax3rs@users.noreply.github.com>
2024-12-24 09:39:26 +0100

    Update InstallProgramsNoPW.sh

--- InstallProgramsNoPW.sh
+++ InstallProgramsNoPW.sh
@@ -10,7 +10,7 @@ wait
 #Should make it so that we have to fill in our PW only once
 read -s -p "Enter Password for sudo: " sudoPW
 #First lets make sure our system is updated
-echo $sudoPW | pacman -Syu --noconfirm
+echo $sudoPW | sudo pacman -Syu --noconfirm
 wait
 #Let's first install yay; a packet manager
 git clone https://aur.archlinux.org/yay.git
@@ -25,9 +25,9 @@ wait
 #Install Brave Browser
 yay -S brave-bin --needed --noconfirm
 #Install Steam
-echo $sudoPW | pacman -S steam --needed --noconfirm
+echo $sudoPW | sudo pacman -S steam --needed --noconfirm
 #Install Discord
-echo $sudoPW | pacman -S discord --needed --noconfirm
+echo $sudoPW | sudo pacman -S discord --needed --noconfirm
 #Install Spotify
 yay -S spotify --needed --noconfirm
 #Install Google Chrome
@@ -35,13 +35,13 @@ yay -S google-chrome --needed --noconfirm
 #Install NordVPN
 yay -S nordvpn-bin --needed --noconfirm
 #Install Teamspeak3
-echo $sudoPW | pacman -S teamspeak3 --needed --noconfirm
+echo $sudoPW | sudo pacman -S teamspeak3 --needed --noconfirm
 #Install Telegram Desktop App
-echo $sudoPW | pacman -S telegram-desktop --needed --noconfirm
+echo $sudoPW | sudo pacman -S telegram-desktop --needed --noconfirm
 #Install Geany (notepad)
-echo $sudoPW | pacman -S geany --needed --noconfirm
+echo $sudoPW | sudo pacman -S geany --needed --noconfirm
 #Install curl
-echo $sudoPW | pacman -S curl --needed --noconfirm
+echo $sudoPW | sudo pacman -S curl --needed --noconfirm
 #Install OpenTabletDriver
 # Downloads the pkgbuild from the AUR.
 git clone https://aur.archlinux.org/opentabletdriver.git
@@ -54,10 +54,10 @@ cd ..
 rm -rf opentabletdriver
 wait
 # Regenerate initramfs
-echo $sudoPW | mkinitcpio -P
+echo $sudoPW | sudo mkinitcpio -P
 wait
 # Unload kernel modules
-echo $sudoPW | rmmod wacom hid_uclogic
+echo $sudoPW | sudo rmmod wacom hid_uclogic
 wait
 #Install 1password
 curl -sS https://downloads.1password.com/linux/keys/1password.asc | gpg --import
