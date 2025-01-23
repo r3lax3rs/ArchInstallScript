f9366d11913091ffc5baeb238de6da56397f5a88
r3lax3rs <124855911+r3lax3rs@users.noreply.github.com>
2025-01-23 16:56:18 +0100

    Update InstallPycharm.sh

--- InstallPycharm.sh
+++ InstallPycharm.sh
@@ -1,1 +1,10 @@
-
+#!/bin/bash
+wget https://download.jetbrains.com/python/pycharm-community-2024.3.1.1.tar.gz
+wait
+sudo tar xzf pycharm-*.tar.gz -C /opt/
+wait
+sudo mv /opt/pycharm-* /opt/pycharm
+wait
+echo 'export PATH=/opt/pycharm/bin:$PATH' >> ~/.bashrc
+wait
+rm -rf pycharm-*
